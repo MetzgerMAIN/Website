9d03e6c59e044f104064dc9e93d0372c1671271f
DzenanB <begovic.dzenan@bene-edu.ch>
2024-04-30 20:13:14 +0200

    Roles_added

---
+++ src/Roles.js
@@ -0,0 +1,29 @@
+import {useKeycloak} from "@react-keycloak/web";
+
+const Roles = {
+    Administrator: "Admin",
+    ShowDozent: "show_dozent",
+    EditDozent: "edit_dozent",
+    ShowKlasse: "show_klasse",
+    EditKlasse: "edit_klasse",
+    ShowMitarbeiter: "show_mitarbeiter",
+    EditMitarbeiter: "edit_mitarbeiter",
+    ShowModul: "show_modul",
+    EditModul: "edit_modul",
+    ShowRaum: "show_raum",
+    EditRaum: "edit_raum",
+    ShowSchueler: "show_schueler",
+    EditSchueler: "edit_schueler",
+}
+
+const IsInRole = (rolesToCheckAgainst) => {
+    const {keycloak, initialized} = useKeycloak()
+
+    if(!Object.keys(Roles).some((role) => rolesToCheckAgainst.includes(role))) return false;
+    if(!initialized) return false;
+    if(!keycloak.authenticated) return false;
+
+    keycloak.tokenParsed.roles.intersects(rolesToCheckAgainst);
+}
+
+export {Roles, IsInRole};
